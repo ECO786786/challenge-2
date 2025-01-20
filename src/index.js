1d0587faa84a4a01cd4e99ffdcea5994f78d4c0a
Emma Collins <lowpee56@hotmail.com>
2025-01-20 00:53:55 +0000

    add format time test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
- //Main work
+const formatTime = (time) => {
+  if (time < 60) {
+    return `${time.toString()}m`;
+  } else {
+    const hours = Math.floor(time / 60);
+    const minutes = time % 60;
+    return minutes ? `${hours}h ${minutes} min` : `${hours}h`;
+  }
+};
+module.exports = formatTime;
